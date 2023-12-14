a78cd96bf7ca765e3db2153429bb0afea3729788
chanbengz <chanbengz@foxmail.com>
2023-12-14 21:15:39 +0800

    Refactor ProblemF.cpp

--- CS203 Data Structures and Algorithm Analysis/Labs/lab8/ProblemF.cpp
+++ CS203 Data Structures and Algorithm Analysis/Labs/lab8/ProblemF.cpp
@@ -1,32 +1,25 @@
 #include<cstdio>
 #include<vector>
 using namespace std;
-
 const int MOD = 1e9 + 7, MAXN = 1e5 + 5;
 int n, m, t, a[MAXN], b[MAXN], f[MAXN];
-vector<int> g[MAXN];
 
 int main() {
     scanf("%d", &t);
     while(t--) {
         scanf("%d%d", &n, &m);
-        for(int i = 0; i < MAXN; i++) { g[i].clear(); f[i] = 0; }
         for(int i = 1; i <= n; i++) scanf("%d%d", &a[i], &b[i]);
+        vector<int> g[n + 1];
         for(int i = 0, u, v; i < m; i++) {
             scanf("%d%d", &u, &v);
             g[v].push_back(u);
         }
         long long sum = 0;
         for(int i = 1; i <= n; i++) {
-            vector<int> ffa, wfa;
-            for(int j = 0; j < g[i].size(); j++) {
-                ffa.push_back(f[g[i][j]]);
-                wfa.push_back(a[g[i][j]]);
-            }
             int cur = 0;
             for(int j = 0; j < g[i].size(); j++) {
-                cur = (cur + wfa[j]) % MOD;
-                cur = (cur + ffa[j]) % MOD;
+                cur = (cur + f[g[i][j]]) % MOD;
+                cur = (cur + a[g[i][j]]) % MOD;
             }
             f[i] = cur;
         }
