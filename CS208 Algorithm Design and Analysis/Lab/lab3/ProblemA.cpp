8d6040386fedaab378333aec97d2f5faf1bddeab
chanbengz <chanbengz@foxmail.com>
2024-03-11 11:47:08 +0800

    fix: complete fixing ProblemA

--- CS208 Algorithm Design and Analysis/Lab/lab3/ProblemA.cpp
+++ CS208 Algorithm Design and Analysis/Lab/lab3/ProblemA.cpp
@@ -3,21 +3,23 @@
 #include <string>
 #include <sstream>
 #include <stack>
-using std::cin;    using std::cout;
-using std::endl;   using std::vector;
-using std::string; using std::stack;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::stack;
 int n, m; string line, word;
-
-class file {
-public:
-    string name, content;
-};
 
 class file_tree {
 public:
-    string name;
-    vector<file *> files;
-    vector<file_tree *> folders;
+    file_tree(string name = ".",
+              string content = "",
+              bool is_file = false)
+    :name(name), content(content), is_file(is_file) {}
+    bool is_file = false;
+    string name, content;
+    vector<file_tree *> files;
 };
 
 file_tree root;
@@ -26,7 +28,7 @@ vector<string> split(string s)
 {
     vector<string> res; string tmp = "";
     for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '/') { res.push_back(tmp); tmp = ""; }
+        if (s[i] == '/' && tmp != "") { res.push_back(tmp); tmp = ""; }
         else { tmp += s[i]; }
     }
     if (tmp != "") res.push_back(tmp);
@@ -35,15 +37,13 @@ vector<string> split(string s)
 
 file_tree *find_folder(vector<string> path, bool prev = true)
 {
-    file_tree *cur = &root;
-    stack<file_tree *> st;
+    file_tree *cur = &root; stack<file_tree *> st;
     for (int i = 0; i < path.size() - prev; i++) {
         if (path[i] == ".") { continue; }
         else if (path[i] == "..") { cur = st.top(); st.pop(); continue; }
-        for (int j = 0; j < cur->folders.size(); j++) {
-            if (cur->folders[j]->name == path[i]) {
-                st.push(cur);
-                cur = cur->folders[j];
+        for (auto &j : cur->files) {
+            if (j->name == path[i]) {
+                st.push(cur); cur = j; break;
             }
         }
     }
@@ -52,15 +52,13 @@ file_tree *find_folder(vector<string> path, bool prev = true)
 
 void echo(vector<string> paras)
 {
-    string content; vector<string> path;
+    string content = ""; vector<string> path;
     for (int i = 1; i < paras.size(); i++) {
         if (paras[i] != ">") { content += paras[i] + (paras[i + 1] == ">" ? "" : " "); }
         else { path = split(paras[i + 1]); break; }
     }
     file_tree *cur = find_folder(path);
-    file *new_file = new file();
-    new_file->name = path[path.size() - 1];
-    new_file->content = content;
+    file_tree *new_file = new file_tree(path.back(), content, true);
     cur->files.push_back(new_file);
 }
 
@@ -68,53 +66,34 @@ void mkdir(vector<string> paras)
 {
     vector<string> path = split(paras[1]);
     file_tree *cur = find_folder(path);
-    file_tree *new_folder = new file_tree();
-    new_folder->name = path[path.size() - 1];
-    cur->folders.push_back(new_folder);
+    file_tree *new_folder = new file_tree(path.back(), "", false);
+    cur->files.push_back(new_folder);
 }
 
 void rm(vector<string> paras)
 {
-    bool folder = false; vector<string> path;
+    vector<string> path;
     for (int i = 1; i < paras.size(); i++) {
-        if (paras[i] == "-rf") { folder = true; }
-        else { path = split(paras[i]); }
+        if (paras[i] != "-rf") { path = split(paras[i]); }
     }
     file_tree *cur = find_folder(path);
-
-    if (folder) {
-        for (int j = 0; j < cur->folders.size(); j++) {
-            if (cur->folders[j]->name == path[path.size() - 1]) {
-                delete cur->folders[j];
-                cur->folders.erase(cur->folders.begin() + j); break;
-            }
-        }
-    } else {
-        for (int j = 0; j < cur->files.size(); j++) {
-            if (cur->files[j]->name == path[path.size() - 1]) {
-                delete cur->files[j];
-                cur->files.erase(cur->files.begin() + j); break;
-            }
+    for (int j = 0; j < cur->files.size(); j++) {
+        if (cur->files[j]->name == path[path.size() - 1]) {
+            delete cur->files[j];
+            cur->files.erase(cur->files.begin() + j); break;
         }
     }
 }
 
 void mv(vector<string> paras)
 {
-    vector<string> src_path = split(paras[1]);
-    file_tree *src = find_folder(src_path);
-    vector<string> dst_path = split(paras[2]);
-    file_tree *dst = find_folder(dst_path, false);
+    vector<string> path = split(paras[1]);
+    file_tree *src = find_folder(path);
+    file_tree *dst = find_folder(split(paras[2]), false);
     for (int i = 0; i < src->files.size(); i++) {
-        if (src->files[i]->name == src_path[src_path.size() - 1]) {
+        if (src->files[i]->name == path.back()) {
             dst->files.push_back(src->files[i]);
-            src->files.erase(src->files.begin() + i); break;
-        }
-    }
-    for (int i = 0; i < src->folders.size(); i++) {
-        if (src->folders[i]->name == src_path[src_path.size() - 1]) {
-            dst->folders.push_back(src->folders[i]);
-            src->folders.erase(src->folders.begin() + i); break;
+            src->files.erase(src->files.begin() + i); return;
         }
     }
 }
@@ -123,53 +102,41 @@ void cat(vector<string> paras)
 {
     vector<string> path = split(paras[1]);
     file_tree *cur = find_folder(path);
-    for (int j = 0; j < cur->files.size(); j++) {
-        if (cur->files[j]->name == path[path.size() - 1]) {
-            cout << cur->files[j]->content << endl;
-            return;
+    for (auto &i : cur->files) {
+        if (i->name == path.back() && i->is_file) {
+            cout << i->content << endl; return;
         }
     }
+    cout << "" << endl;
 }
 
 vector<string> find_res;
 string target, type;
 
-// prefix ends with '/'
 void print_tree(file_tree *cur, string prefix)
 {
-    for (int i = 0; i < cur->files.size(); i++) {
-        if ((type == "" || type == "f") && (target == "" || target == cur->files[i]->name)) {
-            find_res.push_back(prefix + cur->files[i]->name);
-        }
-    }
-    for (int i = 0; i < cur->folders.size(); i++) {
-        if ((type == "" || type == "d") && (target == "" || target == cur->folders[i]->name)) {
-            find_res.push_back(prefix + cur->folders[i]->name);
-        }
-        print_tree(cur->folders[i], prefix + cur->folders[i]->name + "/");
+    if (cur == nullptr) return;
+    bool found = true;
+    if (type == "f" && !cur->is_file)        { found = false; }
+    if (type == "d" && cur->is_file)         { found = false; }
+    if (target != "" && cur->name != target) { found = false; }
+    if (found) find_res.push_back(prefix);
+    for (auto &i : cur->files) {
+        print_tree(i, prefix + "/" + i->name);
     }
 }
 
 void find(vector<string> paras)
 {
     find_res.clear(); target = type = "";
-    vector<string> path = {"."};
-    file_tree *cur; string prefix;
+    string prefix = paras.size() > 1 && paras[1].front() != '-' ? paras[1] : ".";
+    vector<string> path = split(prefix);
     for(int i = 1; i < paras.size(); i++) {
         if (paras[i] == "-name") { target = paras[++i]; }
         else if (paras[i] == "-type") { type = paras[++i]; }
-        else { path = split(paras[i]); prefix = paras[i]; }
     }
-
-    if (prefix == "") {
-        cur = &root; prefix = "./";
-        if ((type == "d" || type == "") && target == "") { find_res.push_back(cur->name); }
-    } else {
-        prefix += prefix[prefix.length() - 1] != '/' ? "/" : "";
-        cur = find_folder(path, false);
-    }
-    print_tree(cur, prefix);
-
+    prefix.back() == '/' ? prefix.pop_back() : void();
+    print_tree(find_folder(path, false), prefix);
     cout << find_res.size() << endl;
     for (int i = 0; i < find_res.size(); i++) { cout << find_res[i] << endl; }
 }
@@ -190,11 +157,7 @@ void process()
 int main()
 {
     std::ios::sync_with_stdio(false);
-    cin >> n >> m;
-    root.name = ".";
-    getline(cin, line);
-    for (int i = 0; i < n; i++) { process(); }
-    for (int i = 0; i < m; i++) { process(); }
-
+    cin >> n >> m; cin.ignore();
+    for (int i = 0; i < n + m; i++) { process(); }
     return 0;
 }
