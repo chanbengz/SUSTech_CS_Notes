6667d30621e288048ca35e800872262ffffb721f
chanbengz <chanbengz@foxmail.com>
2024-04-25 11:56:43 +0800

    fix: FFT template

--- CS203 Data Structures and Algorithm Analysis/Template/Misc/FFT.cpp
+++ CS203 Data Structures and Algorithm Analysis/Template/Misc/FFT.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 using namespace std;
 
-const int MAXN = 1e5 + 5;
-long long x[MAXN], y[MAXN], z[MAXN], n, m;
+const int MAXN = 1e6 + 5;
+long long x[MAXN], y[MAXN], z[2 * MAXN], n, m;
 
 class Complex {
     public:
@@ -59,27 +59,27 @@ void fft(Complex* a, int n, int k, Complex* omega) {
 void multiply() {
     int k = 0;
     while((1 << k) < n + m) k++;
-    int n = 1 << k;
-    Complex* omega = new Complex[n];
-    Complex* omegaInv = new Complex[n];
-    Complex* a = new Complex[n];
-    Complex* b = new Complex[n];
-    for(int i = 0; i < n; i++) {
-        omega[i] = Omega(n, i);
+    int nn = 1 << k;
+    Complex* omega = new Complex[nn];
+    Complex* omegaInv = new Complex[nn];
+    Complex* a = new Complex[nn];
+    Complex* b = new Complex[nn];
+    for(int i = 0; i < nn; i++) {
+        omega[i] = Omega(nn, i);
         omegaInv[i] = omega[i].conj();
         a[i] = i < n ? Complex(x[i], 0) : ZERO;
         b[i] = i < m ? Complex(y[i], 0) : ZERO;
     }
-    fft(a, n, k, omega);
-    fft(b, n, k, omega);
-    for(int i = 0; i < n; i++) a[i] = a[i] * b[i];
-    fft(a, n, k, omegaInv);
-    for(int i = 0; i < n; i++) z[i] = (long long) floor(a[i].real / n + 0.5);
+    fft(a, nn, k, omega);
+    fft(b, nn, k, omega);
+    for(int i = 0; i < nn; i++) a[i] = a[i] * b[i];
+    fft(a, nn, k, omegaInv);
+    for(int i = 0; i < nn; i++) z[i] = (long long) floor(a[i].real / nn + 0.5);
 }
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-    cin >> n >> m;
+    cin >> n >> m; n++, m++;
     for(int i = n - 1; i >= 0; i--) cin >> x[i];
     for(int i = m - 1; i >= 0; i--) cin >> y[i];
     multiply();
